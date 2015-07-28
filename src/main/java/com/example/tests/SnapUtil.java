392708ba9c4c8a9cba1c0c987ca201477aa5f76b
onlywhz <onlywhz@qq.com>
2015-07-28 15:40:53 +0800

    add ie and chrome

--- src/main/java/com/example/tests/SnapUtil.java
+++ src/main/java/com/example/tests/SnapUtil.java
@@ -8,21 +8,34 @@ import java.util.Date;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class SnapUtil {
 	private static int filenum=1;
 	private static String sysDate;
+	public static final String CHROME_DRIVER_HOME_VAULE="D:\\Projects\\selenium-ide\\chromedriver\\chromedriver.exe"; 
+	public static final String CHROME_DRIVER_HOME_KEY="webdriver.chrome.driver";
+	public static final String IE_DRIVER_HOME_VAULE="D:\\Projects\\selenium-ide\\IEDriverServer\\IEDriverServer.exe"; 
+	public static final String IE_DRIVER_HOME_KEY="webdriver.ie.driver";
+	
 	static {
 		SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
 		sysDate=df.format(new Date());
 	}
+	
+	/**
+	 * 
+	 * @param drivername
+	 * Example:SnapUtil.snapshot((TakesScreenshot)driver);
+	 */
 	public static void snapshot(TakesScreenshot drivername)
 	  {
 	    String parentPath = "D:\\Projects\\selenium-ide\\pictures\\";
 	    File workDir=new File(parentPath+sysDate);
 	    workDir.deleteOnExit();
 		workDir.mkdir();
-	    System.out.println(workDir.getPath());
 	    File scrFile = drivername.getScreenshotAs(OutputType.FILE);
 	        try {
 	        	File desFile=new File(workDir.getPath()+"\\"+filenum+".png");
@@ -39,4 +52,12 @@ public class SnapUtil {
 	            System.out.println("screen shot finished");
 	        }
 	  }
+	
+	//ignore Protected Mode
+    public static void ignoreeProtectedMode(){    
+        System.setProperty(IE_DRIVER_HOME_KEY, IE_DRIVER_HOME_VAULE);
+        DesiredCapabilities dc = DesiredCapabilities.internetExplorer();
+        dc.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+        dc.setVersion("7");
+    }
 }
